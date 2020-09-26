4dd3b93d94500ab1c87a7238cfa2f2458c0391d7
Vitor Gois <vitorcgois777@gmail.com>
2020-09-25 23:16:12 -0300

    Otimization with RequestMapping

--- src/main/java/com/example/demo/controller/PedidoController.java
+++ src/main/java/com/example/demo/controller/PedidoController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Indica ao Spring que é um Controlador
 @RestController
+@RequestMapping("/pedidos");
 public class PedidoController {   
 
     //Psiu, coloca o objeto aqui pra mim
@@ -25,7 +27,7 @@ public class PedidoController {
     private PedidoRepository repository;    //cria um atributo que armazenará um objeto do tipo PedidoRepository
 
     //Método responsável por fazer o cadastro um pedido
-    @PostMapping("/pedidos")
+    @PostMapping("/")
     public ResponseEntity<Void> save(@RequestBody Pedido pedido) {    //Void pq é um post não precisa retornar nada, além dos status http
         Pedido ped = repository.save(pedido);
 
@@ -35,13 +37,13 @@ public class PedidoController {
     }
 
     //Método responsável por mostrar todos os pedidos
-    @GetMapping("/pedidos")
+    @GetMapping("/")
     public List<Pedido> getPedidos() {
         return repository.getAllPedidos();
     }
 
     //Método responsável por mostrar determinada pedido
-    @GetMapping("/pedidos/{codigo}")
+    @GetMapping("/{codigo}")
     public ResponseEntity<Pedido> getPedidoByCodigo(@PathVariable int codigo) {
         Pedido ped = repository.getPedidoByCod(codigo);
 
@@ -54,7 +56,7 @@ public class PedidoController {
     }
 
     //Método responsável por deletar um pedido
-    @DeleteMapping("/pedidos/{codigo}")
+    @DeleteMapping("/{codigo}")
     public ResponseEntity<Void> removePedido(@PathVariable int codigo) {
         Pedido ped = repository.getPedidoByCod(codigo);
 
@@ -68,7 +70,7 @@ public class PedidoController {
     }
 
     //Método responsável por alterar um pedido
-    @PutMapping("/pedidos/{codigo}")
+    @PutMapping("/{codigo}")
     public ResponseEntity<Pedido> modifyPedido(@PathVariable int codigo, @RequestBody Pedido pedido) {
         //Existe um pedido com o id passado?
         if (repository.getPedidoByCod(codigo) != null) {
